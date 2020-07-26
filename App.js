e56a3052817783469c9f1b096027e5c49661a532
Ana Sofia Lau Hou <alau@ubiqua.me>
2020-07-25 20:08:13 -0500

    Render list

--- App.js
+++ App.js
@@ -1,22 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
+// import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Main from './components/MainComponent'
+// import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello Ana!! Good night :)</Text>
-      <Text>Lets learn React!! </Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  // return (
+  //   <View style={styles.container}>
+  //     <Text>Hello Ana!! Good night :)</Text>
+  //     <Text>Lets learn React!! </Text>
+  //     <StatusBar style="auto" />
+  //   </View>
+  // );
+
+    return (
+      <Main />
+    );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#0ff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#0ff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
