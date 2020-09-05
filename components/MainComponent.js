61d2cd2b7d964dce0c68b7940e413b5892635bf4
Ana Lau <anasofialauhou@hotmail.com>
2020-09-05 13:27:26 -0500

    Add swipeout button and alerts for deleteing favotites

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -6,6 +6,7 @@ import Home from './HomeComponent';
 import Contact from './ContactComponent';
 import About from './AboutComponent';
 import Reservation from './ReservationComponent';
+import Favorites from './FavoriteComponent';
 import { createAppContainer, SafeAreaView } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createDrawerNavigator, DrawerItems } from 'react-navigation-drawer'
@@ -102,6 +103,23 @@ const AboutUsNavigator = createStackNavigator({
     }
 );
 
+const FavoritesNavigator = createStackNavigator({
+  Favorites: { screen: Favorites }
+}, {
+  navigationOptions: ({ navigation }) => ({
+    headerStyle: {
+        backgroundColor: "#512DA8"
+    },
+    headerTitleStyle: {
+        color: "#fff"
+    },
+    headerTintColor: "#fff",
+    headerLeft: <Icon name="menu" size={24}
+      iconStyle={{ color: 'white' }}
+      onPress={ () => navigation.navigate('DrawerToggle') } />
+  })
+})
+
 const ReservationNavigator = createStackNavigator({
   Reservation: { screen: Reservation }
 }, {
@@ -194,6 +212,21 @@ const MainNavigator = createDrawerNavigator({
         color={tintColor}
         />
       )
+    }
+  },
+  Favorites: {
+    screen: FavoritesNavigator,
+    navigationOptions: {
+      title: 'My Favorites',
+      drawerLabel: 'My Favorites',
+      drawerIcon: ({ tintColor, focused }) => (
+        <Icon
+          name='heart'
+          type='font-awesome'
+          size={24}
+          iconStyle={{ color: tintColor }}
+        />
+      ),
     }
   },
   Reservation: {
