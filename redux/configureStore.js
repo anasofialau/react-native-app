82ee4608c43ff9054b249a9d5798b02818eb3f47
Ana Lau <anasofialauhou@hotmail.com>
2020-09-05 17:28:18 -0500

    Redux persistance storage

--- redux/configureStore.js
+++ redux/configureStore.js
@@ -6,18 +6,28 @@ import { comments } from './comments';
 import { promotions } from './promotions';
 import { leaders } from './leaders';
 import { favorites } from './favorites';
+import { persistStore, persistCombineReducers } from 'redux-persist';
+import { AsyncStorage } from 'react-native';
+
+const config = {
+  key: 'root',
+  storage: AsyncStorage,
+  debug: true
+}
 
 export const ConfigureStore = () => {
     const store = createStore(
-        combineReducers({
-            dishes,
-            comments,
-            promotions,
-            leaders,
-            favorites
-        }),
-        applyMiddleware(thunk, logger)
+    persistCombineReducers(config, {
+          dishes,
+          comments,
+          promotions,
+          leaders,
+          favorites
+      }),
+      applyMiddleware(thunk, logger)
     );
 
-    return store;
+    const persistor = persistStore(store)
+
+    return { persistor, store };
 }
